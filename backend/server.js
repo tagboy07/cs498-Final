3fe59a4344ff95145cd9e9b333b3f358acd93cb7
ZohairZaman <zohairzaman14@gmail.com>
2017-12-09 20:21:24 -0600

    Minor fixes

--- backend/server.js
+++ backend/server.js
@@ -45,8 +45,16 @@ app.use(passport.session());
 var allowCrossDomain = function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-    next();
+
+    if ('OPTIONS' === req.method) {
+      //respond with 200
+      res.send(200);
+    }
+    else {
+        next();
+    }
 };
 app.use(allowCrossDomain);
 
