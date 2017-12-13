f5f0f317d56ea679fbc26510472e7dbdee3ff7c6
ZohairZaman <zohairzaman14@gmail.com>
2017-12-13 04:32:46 -0600

    lol

--- source/components/Home/Home.jsx
+++ source/components/Home/Home.jsx
@@ -8,7 +8,7 @@ import PropTypes from 'prop-types';
 import { Container, Divider, Grid, Input } from 'semantic-ui-react'
 import Header from '../Header/Header.jsx';
 import styles from './Home.scss'
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+//import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 class Home extends Component {
 
