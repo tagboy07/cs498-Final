af4c3626befd2ff919b3ea2a6342cbb1bd68aa2d
Muhammad Singdi <muhammadsingdi@wirelessprv-10-194-100-202.near.illinois.edu>
2017-12-12 21:55:51 -0600

    profile page

--- source/index.jsx
+++ source/index.jsx
@@ -11,6 +11,7 @@ import Login from './components/Login/Login.jsx';
 import Header from './components/Header/Header.jsx';
 import Register from './components/Register/Register.jsx';
 import Review from './components/Review/Review.jsx';
+import Profile from './components/Profile/Profile.jsx'
 
 // Include any new stylesheets here
 // Note that components' stylesheets should NOT be included here.
@@ -24,6 +25,7 @@ render(
 
         <Route exact path="/" component={Home} />
         <Route path="/login" component={Login} />
+        <Route path="/profile" component={Profile} />
         <Route path="/register" component={Register} />
         <Route path="/header" component={Header} />
         <Route path="/class/:id" component={Class} />
