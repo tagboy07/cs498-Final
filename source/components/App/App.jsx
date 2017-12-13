703fcdf84b682547683930008572bf5029e7af7b
Matthew Cheung <mcheung3@illinois.edu>
2017-12-12 19:28:50 -0600

    asdf

---
+++ source/components/App/App.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom';
+import { Button } from 'semantic-ui-react'
+import axios from 'axios'
+import PropTypes from 'prop-types';
+import {render} from 'react-dom';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import { Container, Divider, Grid, Image } from 'semantic-ui-react'
+
+
+import Home from '../Home/Home.jsx';
+import Class from '../Class/Class.jsx';
+import Login from '../Login/Login.jsx';
+import Header from '../Header/Header.jsx';
+import Register from '../Register/Register.jsx';
+import Review from '../Review/Review.jsx';
+
+
+class App extends Component {
+
+
+	render(){
+
+		return( 
+				<Router>
+			      <div className="wrapper">
+			        <Header></Header>
+
+			        <Route exact path="/" component={Home} />
+			        <Route path="/login" component={Login} />
+			        <Route path="/register" component={Register} />
+			        <Route path="/header" component={Header} />
+			        <Route path="/class/:id" component={Class} />
+			        <Route path="/review/" component={Review} />
+			      </div>
+			    </Router>
+	    	)
+
+	}
+
+}
+
+export default App;
